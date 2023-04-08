d328048ed882e493d00851cbd214b72cbf74f7e1
Aman-ullah-87 <khaman2645@gmail.com>
2023-04-09 05:49:45 +0600

    shop-page-design

--- src/components/Card/ShopCard.jsx
+++ src/components/Card/ShopCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const ShopCard = () => {
+const ShopCard = ({product}) => {
+    const {name,picture,price,category}=product
     return (
         <div className='shadow-lg  lg:max-w-sm bg-[#f1f2f4] object-center p-6'>
-            <img src="/src/assets/images/smart-home-v2-7.jpg" alt="" className='lg:h-[320px] lg:w-[336px] w-full h-[14rem] cover  flex justify-center object-cover' />
-            <h5 className='text-2xl font-bold my-2'>Smart Air Conditioner</h5>
-            <p>Category: Home Appliance</p>
-            <p className='font-bold text-md'>Price: 799$</p>
+            <img src={picture} alt="" className='lg:h-[320px] lg:w-[336px] w-full h-[14rem] cover  flex justify-center object-cover' />
+            <h5 className='text-2xl font-bold my-2'>{name}</h5>
+            <p>Category: {category}</p>
+            <p className='font-bold text-md'>Price: {price}$</p>
             <button className='btn !w-full bg-cyan-200 hover:bg-cyan-400'>Add To Cart</button>
         </div>
     );
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ShopCard from '../components/Card/ShopCard';
+import { useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
+    const products = useLoaderData()
+    
     return (
-        <div className='lg:px-32 xl:px-32 px-12 my-14'>
-        <ShopCard />
+        <div className='lg:px-32 xl:px-32 px-12 my-14 grid md:gird-cols-2 lg:gird-cols-3 xl:grid-cols-3 gap-8'>
+
+            {
+                products.map(product=> <ShopCard key={product.id} product={product} />)
+            }
+        
         </div>
     );
 };
